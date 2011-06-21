814025c362bacc5abf322035885a504ef76b390f
Franz Wilding <franzwilding@Franz-Wildings-MacBook-Pro.local>
2011-06-21 09:43:27 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contact2::Application.config.secret_token = 'd4abe25af82fca6627b8036e37a72e2076faf29ade73ccd393389dbd7168b8e4d322f34c07f2fc14c88dba658e0f8feee37c7cd6b50cf836fde9e8bd70d1f2c8'
