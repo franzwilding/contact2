d78c358263b44c7465d604c93665f618ebee13be
Franz Wilding <franzwilding@Franz-Wildings-MacBook-Pro.local>
2011-10-05 19:20:10 +0200

    ltl migration_fix

--- db/migrate/20111003190148_rename_orderby_column_in_field.rb
+++ db/migrate/20111003190148_rename_orderby_column_in_field.rb
@@ -1,11 +1,11 @@
 class RenameOrderbyColumnInField < ActiveRecord::Migration
   def self.up
     add_column :fields, :position, :integer
-    remove_column :lists, :orderby
+    #remove_column :lists, :orderby
   end
 
   def self.down
     remove_column :fields, :position
-    add_column :lists, :orderby, :integer
+    #add_column :lists, :orderby, :integer
   end
 end
